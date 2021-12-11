1f88c7342fad56010a236e829e77b50c5cc28292
czc222 <1263291450@qq.com>
2021-12-11 18:57:17 +0800

    基本完成官网框架

---
+++ src/components/DialogDemo.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>这是dialogDemo</div>
+</template>
+<script lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/TabsDemo.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>这是tabsDemo</div>
+</template>
+<script lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
