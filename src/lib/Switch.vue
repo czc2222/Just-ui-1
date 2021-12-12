fd377604f3ee5d343caa264decb293b7513ce643
czc222 <1263291450@qq.com>
2021-12-12 13:33:44 +0800

    switch css 2

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -25,7 +25,7 @@ button{
   height: $h;
   width: $h*2;
   border: none;
-  background: grey;
+  background: #bfbfbf;
   border-radius: $h/2;
   position: relative;
 }
@@ -37,15 +37,21 @@ span{
   width: $h2;
   background:white;
   border-radius: $h2 / 2;
-  transition:left 250ms;
+  transition: all 250ms;
 }
 button.checked {
-  background: blue;
+  background: #1085fd;
 }
 button.checked >span{
   left:calc(100% - #{$h2} - 2px)
 }
 button:focus{
   outline: none;
+}
+button:active{
+  > span {width: $h2 + 4px;}
+}
+button.checked:active{
+  > span {width: $h2 + 4px; margin-left: -4px;}
 }
 </style>
