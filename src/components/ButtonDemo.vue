6cb033995fe20a329648aa074ab4f5e7b78534a1
czc222 <1263291450@qq.com>
2021-12-12 14:36:15 +0800

    创建button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,8 +1,15 @@
 <template>
-  <div>这是buttonDemo</div>
+  <div>
+    <Button>你好</Button>
+  </div>
 </template>
 <script lang="ts">
-
+import Button from '../lib/Button.vue'
+export default {
+  components:{
+    Button,
+  }
+}
 </script>
 
 <style lang="scss" scoped>
---
+++ src/lib/Button.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <button>
+      <slot/>
+    </button>
+  </div>
+</template>
+<script lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
