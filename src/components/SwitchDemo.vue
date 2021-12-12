17287af7796307a4a92ac4fa68c5e398a78573e0
czc222 <1263291450@qq.com>
2021-12-12 12:10:08 +0800

    Switch css 1

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,8 +1,13 @@
 <template>
-  <div>这是switchDemo</div>
+  <Switch/>
 </template>
 <script lang="ts">
-
+import Switch from '../lib/Switch.vue'
+export default {
+   components:{
+     Switch,
+   }
+}
 </script>
 
 <style lang="scss" scoped>
