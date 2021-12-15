01bef553bff8deb7cc4b2cc6b6e8387f3bbf969c
czc222 <1263291450@qq.com>
2021-12-15 16:44:30 +0800

    支持 loading 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>Button 示例</div>
   <h1>示例1</h1>
   <div>
     <Button>你好</Button>
@@ -48,6 +49,11 @@
     <Button disabled>禁用按钮</Button>
     <Button theme="link" disabled>禁用链接按钮</Button>
     <Button theme="text" disabled>禁用按钮</Button>
+  </div>
+  <h1>示例5</h1>
+  <div>
+    <Button loading>加载中</Button>
+    <Button >加载完毕</Button>
   </div>
 </template>
 <script lang="ts">
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,6 @@
 <template>
   <button class="gulu-button" :class="classes" :disabled="disabled">
+    <span v-if="loading" class="gulu-loadingIndicator"></span>
     <slot/>
   </button>
 </template>
@@ -24,6 +25,10 @@ export default {
       type: Boolean,
       default: false,
     },
+    loading: {
+      type: Boolean,
+      default: false
+    }
 
   },
   setup(props) {
@@ -176,5 +181,21 @@ $grey: grey;
       color: $grey;
     }
   }
+  > .gulu-loadingIndicator{
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px;
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: gulu-spin 1s infinite linear;
+  }
+}
+@keyframes gulu-spin {
+  0%{transform: rotate(0deg)}
+  100%{transform: rotate(360deg)}
+
 }
 </style>
