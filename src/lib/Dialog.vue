8858431f8d11b3c17833b15bbba90e79eba8953e
czc222 <1263291450@qq.com>
2021-12-15 19:39:19 +0800

    使用Teleport

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,21 +1,24 @@
 <template>
   <template v-if="visible">
-    <div class="gulu-dialog-overlay" @click="closeClickOverlay"></div>
-    <div class="gulu-dialog-wrapper">
-      <div class="gulu-dialog">
-        <header>
-          <slot name="title"/>
-          <span class="gulu-dialog-close" @click="close"></span>
-        </header>
-        <main>
-          <slot name="content"/>
-        </main>
-        <footer>
-          <Button level="main" @click="ok">OK</Button>
-          <Button @click="cancel">Cancel</Button>
-        </footer>
+    <Teleport to="body">
+      <div class="gulu-dialog-overlay" @click="closeClickOverlay"></div>
+      <div class="gulu-dialog-wrapper">
+        <div class="gulu-dialog">
+          <header>
+            <slot name="title"/>
+            <span class="gulu-dialog-close" @click="close"></span>
+          </header>
+          <main>
+            <slot name="content"/>
+          </main>
+          <footer>
+            <Button level="main" @click="ok">OK</Button>
+            <Button @click="cancel">Cancel</Button>
+          </footer>
+        </div>
       </div>
-    </div>
+    </Teleport>
+
   </template>
 
 </template>
