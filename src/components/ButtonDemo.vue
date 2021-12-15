9d042e9bd5a00bbc82c0468eb93941bf297c6e5c
czc222 <1263291450@qq.com>
2021-12-15 16:23:45 +0800

    支持 size 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,24 +1,34 @@
 <template>
+  <h1>示例1</h1>
   <div>
     <Button>你好</Button>
     <Button theme="button">你好</Button>
     <Button theme="link">你好</Button>
     <Button theme="text">你好</Button>
   </div>
+
+  <h1>示例2</h1>
+  <div>
+    <Button size="big">大大大</Button>
+    <Button>普普通</Button>
+    <Button size="small">小小小</Button>
+  </div>
+
 </template>
 <script lang="ts">
-import Button from '../lib/Button.vue'
+import Button from '../lib/Button.vue';
+
 export default {
-  components:{
+  components: {
     Button,
   },
-  setup(){
-     const onClick =()=>{
-       console.log('hi');
-     }
-     return {onClick}
+  setup() {
+    const onClick = () => {
+      console.log('hi');
+    };
+    return {onClick};
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
