a63921ba835536ab767c1f1443a9ccea430c2860
czc222 <1263291450@qq.com>
2021-12-15 16:31:29 +0800

    支持 level 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,34 +1,40 @@
 <template>
   <button class="gulu-button" :class="classes">
-    <slot />
+    <slot/>
   </button>
 </template>
 <script lang="ts">
-import {computed} from 'vue'
+import {computed} from 'vue';
+
 export default {
-  props:{
-    theme:{
-      type:String,
-      default:'button'
+  props: {
+    theme: {
+      type: String,
+      default: 'button'
     },
-    size:{
-      type:String,
-      default:'normal'
-    }
+    size: {
+      type: String,
+      default: 'normal'
+    },
+    level: {
+      type: String,
+      default: 'normal',
+    },
 
   },
-  setup(props){
-    const {theme,size}=props
-    const classes = computed(()=>{
-      return{
-      [`gulu-theme-${theme}`]: theme,
-      [`gulu-size-${size}`]: size
-      }
-    })
-    return {classes}
+  setup(props) {
+    const {theme, size, level} = props;
+    const classes = computed(() => {
+      return {
+        [`gulu-theme-${theme}`]: theme,
+        [`gulu-size-${size}`]: size,
+        [`gulu-level-${level}`]: level,
+      };
+    });
+    return {classes};
   }
 
-}
+};
 </script>
 
 <style lang="scss">
@@ -37,6 +43,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 .gulu-button {
   box-sizing: border-box;
   height: $h;
@@ -51,6 +58,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
 
   & + & {
     margin-left: 8px;
@@ -69,31 +77,84 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
-  &.gulu-theme-link{
+
+  &.gulu-theme-link {
     border-color: transparent;
     box-shadow: none;
     color: $blue;
-    &:hover,&:focus{
+
+    &:hover, &:focus {
       color: lighten($blue, 10%);
     }
   }
-  &.gulu-theme-text{
+
+  &.gulu-theme-text {
     border-color: transparent;
     box-shadow: none;
     color: inherit;
-    &:hover,&:focus{
+
+    &:hover, &:focus {
       background: darken(white, 5%);;
     }
   }
+
   &.gulu-size-big {
     font-size: 24px;
     height: 48px;
     padding: 0 16px;
   }
+
   &.gulu-size-small {
     font-size: 12px;
     height: 20px;
     padding: 0 4px;
+  }
+  &.gulu-theme-button {
+    &.gulu-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    &.gulu-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.gulu-theme-link {
+    &.gulu-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.gulu-theme-text {
+    &.gulu-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.gulu-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
   }
 }
 </style>
