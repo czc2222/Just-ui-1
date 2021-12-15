762fe89abc1b94f53191b4ac324ff607f959a29a
czc222 <1263291450@qq.com>
2021-12-15 23:35:21 +0800

    优化代码

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -5,7 +5,7 @@
            v-for="(t,index) in titles" :key="index"
            :class="{selected:t === selected}"
            @click="select(t)"
-           :ref="el=>{if(el) navItems[index] =el}"
+           :ref="el=>{if(t===selected) selectedItem=el}"
       >{{t}}</div>
       <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
     </div>
@@ -20,7 +20,7 @@
 </template>
 <script lang="ts">
 import Tab from './Tab.vue';
-import {onMounted, onUpdated, ref} from 'vue';
+import {onMounted, ref, watchEffect} from 'vue';
 
 
 export default {
@@ -30,21 +30,21 @@ export default {
     }
   },
   setup(props, context) {
-    const navItems =ref<HTMLDivElement[]>([])
+    const selectedItem =ref<HTMLDivElement>(null)
     const indicator =ref<HTMLDivElement>(null)
     const container =ref<HTMLDivElement>(null)
-    const x=()=>{
-      const divs =navItems.value
-      const result =divs.filter(div=>div.classList.contains('selected'))[0]
-      const {width}=result.getBoundingClientRect()
-      indicator.value.style.width = width + 'px'
-      const {left:left1} = container.value.getBoundingClientRect()
-      const {left:left2}  =result.getBoundingClientRect()
-      const left = left2 - left1
-      indicator.value.style.left =left +'px'
-    }
-    onMounted(x)
-    onUpdated(x)
+    onMounted(()=>{
+      watchEffect(()=>{
+        const {width}=selectedItem.value.getBoundingClientRect()
+        indicator.value.style.width = width + 'px'
+        const {left:left1} = container.value.getBoundingClientRect()
+        const {left:left2}  =selectedItem.value.getBoundingClientRect()
+        const left = left2 - left1
+        indicator.value.style.left =left +'px'
+      })
+    })
+
+
     const defaults = context.slots.default();
 
     defaults.forEach((tag) => {
@@ -60,7 +60,7 @@ export default {
       context.emit('update:selected',title)
     }
 
-    return {defaults,titles,select,navItems,indicator,container};
+    return {defaults,titles,select,selectedItem,indicator,container};
   }
 };
 </script>
