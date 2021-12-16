5b0fcdf46f66674a440b4de742034d0a39db9dca
czc222 <1263291450@qq.com>
2021-12-16 13:40:36 +0800

    使用 iconfont.cn 添加 logo 和 icon

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="topnav">
-    <div class="logo" >LOGO</div>
+    <div class="logo" >
+      <svg class="icon" >
+        <use xlink:href="#icon-logo"></use>
+      </svg>
+    </div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
@@ -40,6 +44,10 @@ $color: #007974;
   > .logo {
     max-width: 6em;
     margin-right: auto;
+    >svg {
+      width: 32px;
+      height: 32px;
+    }
   }
   > .menu {
     display: flex;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,26 @@
 <template>
-  <div class="topnavAndBanner">
-    <TopNav/>
-    <div class="banner">
-      <h1>轱辘UI</h1>
-      <h2>一个厉害的UI框架</h2>
-      <p class="actions">
-        <a href="https://github.com/czc2222">GitHub</a>
-        <router-link to="/Doc">开始</router-link>
-      </p>
+  <div>
+    <div class="topnavAndBanner">
+      <Topnav />
+      <div class="banner">
+        <h1>轱辘UI</h1>
+        <h2>一个厉害的 UI 框架</h2>
+        <p class="actions">
+          <a href="https://github.com">GitHub</a>
+          <router-link to="/doc">开始</router-link>
+        </p>
+      </div>
+    </div>
+    <div class="features">
+      <svg class="icon">
+        <use xlink:href="#icon-vue"></use>
+      </svg>
+      <svg class="icon">
+        <use xlink:href="#icon-ts"></use>
+      </svg>
+      <svg class="icon">
+        <use xlink:href="#icon-light"></use>
+      </svg>
     </div>
   </div>
 </template>
@@ -25,6 +38,12 @@ $border-radius: 4px;
 $color: #007974;
 .topnavAndBanner {
   background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
+}
+.features {
+  >svg {
+    width: 64px;
+    height: 64px;
+  }
 }
 .banner {
   color: $color;
