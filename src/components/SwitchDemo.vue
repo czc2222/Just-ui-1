1912b7ae71f73b4af5f1ff5e9a8285e8350f7f2d
czc222 <1263291450@qq.com>
2021-12-19 17:23:56 +0800

    使用 component

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -4,7 +4,7 @@
     <div class="demo">
       <h2>常规用法</h2>
       <div class="demo-component">
-        <Switch1Demo/>
+        <component :is="Switch1Demo" />
       </div>
       <div class="demo-actions">
         <Button>查看代码</Button>
@@ -16,7 +16,7 @@
     <div class="demo">
       <h2>支持 disabled </h2>
       <div class="demo-component">
-        <Switch2Demo/>
+        <component :is="Switch2Demo" />
       </div>
       <div class="demo-actions">
         <Button>查看代码</Button>
@@ -41,8 +41,6 @@ export default {
     return{ bool,Switch1Demo,Switch2Demo}
   },
    components:{
-     Switch2Demo,
-     Switch1Demo,
      Switch,
      Button
    }
